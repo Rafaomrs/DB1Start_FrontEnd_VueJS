4006639adae849e0c60404a8500d345c819f8b17
Rafael <rafaamattos@hotmail.com>
2019-12-05 15:28:55 -0300

    Utilizando metodos

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      aula: 'FRONT END COM VUEJS',
+      states: []
+    },
+    methods: {
+        changeInputValue(event){
+            console.log(event)
+            this.message = event.target.value;
+        },
+        getStates(){
+            this.states = ['parana', 'sao paulo']
+        }
+    },
+    created(){
+        console.log('criou componente')
+        this.getStates()
     }
   })
